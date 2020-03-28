b4a6767601fcc0147fa81fce530ff0a8f1b77411
rahul <rahulraz1308@gmail.com>
2020-03-28 07:52:02 +0530

    Updates

---
+++ precache-manifest.edf1cae444225e1faebbcb0158db3777.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d868c51287193fa56de8425053dcbad2",
+    "url": "/mogley-clone/index.html"
+  },
+  {
+    "revision": "4ca60117d8d661b1811e",
+    "url": "/mogley-clone/static/js/2.f7fed632.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/mogley-clone/static/js/2.f7fed632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8ac493c40ad0e318b7a",
+    "url": "/mogley-clone/static/js/main.f39712a7.chunk.js"
+  },
+  {
+    "revision": "16971e39434d54fd93dd",
+    "url": "/mogley-clone/static/js/runtime-main.21d861d9.js"
+  },
+  {
+    "revision": "1692c1c3c0e426ad348361ec1a28fcca",
+    "url": "/mogley-clone/static/media/logo.1692c1c3.png"
+  },
+  {
+    "revision": "8b5eda98d5f043938353f85edc9297c5",
+    "url": "/mogley-clone/static/media/pic_1.8b5eda98.jpg"
+  },
+  {
+    "revision": "4970b50abd7c71ab1cc67fa6a795376f",
+    "url": "/mogley-clone/static/media/pic_2.4970b50a.jpg"
+  },
+  {
+    "revision": "86deafdcce9d675ad6220b940b47167d",
+    "url": "/mogley-clone/static/media/pic_3.86deafdc.jpg"
+  },
+  {
+    "revision": "ccb0408addfcdc9b3f6e0256b98f548b",
+    "url": "/mogley-clone/static/media/pic_4.ccb0408a.jpg"
+  },
+  {
+    "revision": "42b1bb2ad41ed2dad7e941a54ce9791e",
+    "url": "/mogley-clone/static/media/pic_5.42b1bb2a.jpg"
+  },
+  {
+    "revision": "15d9f3053f5276339f646a19aa70e3f4",
+    "url": "/mogley-clone/static/media/pic_6.15d9f305.jpg"
+  },
+  {
+    "revision": "0a7ee16f04ca91ab8de86b663d3a6df7",
+    "url": "/mogley-clone/static/media/pic_7.0a7ee16f.jpg"
+  },
+  {
+    "revision": "2d1a29d5faaa9f80a274aed6fec24d3b",
+    "url": "/mogley-clone/static/media/pic_8.2d1a29d5.jpg"
+  },
+  {
+    "revision": "4135577b3d302b9ba237a4aeda9abffd",
+    "url": "/mogley-clone/static/media/pic_9.4135577b.jpg"
+  },
+  {
+    "revision": "626a4e665d1d6dfd48b7b46c279ae9d8",
+    "url": "/mogley-clone/static/media/pic_large_1.626a4e66.jpg"
+  },
+  {
+    "revision": "d2d91d10199898e3f2111f1d1dbb1b21",
+    "url": "/mogley-clone/static/media/pic_large_2.d2d91d10.jpg"
+  },
+  {
+    "revision": "446904aa53e834e290d89c7b7440005a",
+    "url": "/mogley-clone/static/media/pic_large_3.446904aa.jpg"
+  },
+  {
+    "revision": "e863f457b78d1fa18d14903efc87560c",
+    "url": "/mogley-clone/static/media/pic_large_4.e863f457.jpg"
+  }
+]);
